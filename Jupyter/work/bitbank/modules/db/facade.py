e3b00b8e4b984ffbe0822079849f36acaf6ab0a6
yamaguchi-teppei <zuuuubo.tetsu@outlook.jp>
2018-10-12 01:58:16 +0900

    [update]受付が部署を選択する

--- Jupyter/work/bitbank/modules/db/facade.py
+++ Jupyter/work/bitbank/modules/db/facade.py
@@ -1,1 +1,25 @@
+from modules.db import gadept, fitfuncdept, exptdept, exptlogsdept
 
+
+class Facade:
+
+    def __init__(self, host):
+        self._tables = [
+            'genetic_algorithms',
+            'fitness_functions',
+            'experiments',
+            'experiment_logs',
+        ]
+        self._host = host
+
+    def select_department(self, table):
+        if table is self._tables[0]:
+            return gadept.GeneticAlgorithmsDepartment(self._host)
+        elif table is self._tables[1]:
+            return fitfuncdept.FitnessFunctionsDepartment(self._host)
+        elif table is self._tables[2]:
+            return exptdept.ExperimentsDepartment(self._host)
+        elif table is self._tables[3]:
+            return exptlogsdept.ExperimentLogsDepartment(self._host)
+        else:
+            raise ValueError("table '" + table + "' is not found")
