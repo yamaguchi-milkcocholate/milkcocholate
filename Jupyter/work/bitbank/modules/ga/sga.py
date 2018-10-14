fadb7c98631fc428ba498486e2b0aa9211f96723
Yamaguchi Teppei <zuuuubo.tetsu@outlook.jp>
2018-10-14 14:19:46 +0900

    [update] SimpleGeneticAlgoritmクラスのsituationを書き換え

--- Jupyter/work/bitbank/modules/ga/sga.py
+++ Jupyter/work/bitbank/modules/ga/sga.py
@@ -11,6 +11,14 @@ class SimpleGeneticAlgorithm:
     """
 
     def __init__(self, situation, fitness_function, population, mutation, cross, elite_num):
+        """
+        :param situation:          genofeature.Situation      特徴量を表すクラスのインスタンス
+        :param fitness_function:   fitnessfunction.object     適応度関数を表すクラスのインスタンス
+        :param population:         int                        個体数
+        :param mutation:           int                        突然変異を起こすパーセンテージ
+        :param cross:              int                        交叉が起きるパーセンテージ
+        :param elite_num:          int                        次の世代に残すエリートの個体数
+        """
         self.ga = ga.GeneticAlgorithm(mutation, cross, situation, elite_num=elite_num, population=population)
         self.fitness_function = fitness_function
         self.population = population
@@ -25,7 +33,6 @@ class SimpleGeneticAlgorithm:
         """
         学習
         :param steps: 世代交代数
-        :return:
         """
         self.init_population()
         self.generation(steps)
@@ -33,7 +40,6 @@ class SimpleGeneticAlgorithm:
     def init_population(self):
         """
         遺伝子初期化
-        :return:
         """
         self.geno_type = self.ga.init_population()
 
@@ -41,7 +47,6 @@ class SimpleGeneticAlgorithm:
         """
         世代交代
         :param steps: 世代交代数
-        :return:
         """
         self.geno_type, self.fitness = self.ga.generation(steps, self.geno_type, self.fitness_function, self)
 
@@ -54,13 +59,13 @@ class SimpleGeneticAlgorithm:
         """
         sum_fitness = 0
         population = geno_type.shape[0]
-        situations = geno_type.shape[1]
+        feature_num = geno_type.shape[1]
         field = []
         for pop_i in range(population):
             sum_fitness += fitness[pop_i]
             field.append(sum_fitness)
         field = np.asarray(field, int)
-        new_geno_type = np.empty((0, situations), int)
+        new_geno_type = np.empty((0, feature_num), int)
         elites = self.ga.select_elites(geno_type, fitness)
 
         for geno_i in range(0, population):
@@ -73,18 +78,19 @@ class SimpleGeneticAlgorithm:
             if rand >= self.cross:
                 continue
             pair_i = geno_i + 1
-            cut_point = random.randrange(0, situations - 1)
+            cut_point = random.randrange(0, feature_num - 1)
             geno_type[geno_i] = np.asarray(np.r_[new_geno_type[geno_i][:cut_point + 1],
                                                  new_geno_type[pair_i][cut_point + 1:]], int)
             geno_type[pair_i] = np.asarray(np.r_[new_geno_type[pair_i][:cut_point + 1],
                                                  new_geno_type[geno_i][cut_point + 1:]], int)
 
+        range_tuple_list = self.situation.range_to_tuple_list()
         for geno_i in range(self.elite_num, population):
-            for situ_i in range(situations):
+            for situ_i in range(feature_num):
                 rand = random.randrange(0, 100)
                 if rand >= self.mutation:
                     continue
-                value = self.situation[situ_i]
+                value = range_tuple_list[situ_i]
                 geno_type[geno_i][situ_i] = random.randrange(value[0], value[1])
 
         rest = geno_type[self.elite_num:]
@@ -94,7 +100,6 @@ class SimpleGeneticAlgorithm:
     def save_geno_type(self):
         """
         遺伝子の保存
-        :return:
         """
         fitness_function = self.fitness_function.__class__.__name__
         ga_name = self.__class__.__name__
