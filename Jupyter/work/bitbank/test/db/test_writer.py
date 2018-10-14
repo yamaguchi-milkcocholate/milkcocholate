e5cce0f846b4d3d6b320a84cf8494c28ea5f2f5e
Yamaguchi Teppei <zuuuubo.tetsu@outlook.jp>
2018-10-14 12:43:56 +0900

    [update] DB挿入テストでタイムスタンプとpickleのバイナリー型を追加

--- Jupyter/work/bitbank/test/db/test_writer.py
+++ Jupyter/work/bitbank/test/db/test_writer.py
@@ -1,23 +1,36 @@
 import os
 import sys
 import unittest
+import pickle
+import datetime
 sys.path.append(os.path.pardir + '/../')
 from modules.db import writer
+from modules.situation import situation
 
 
 class TestWriter(unittest.TestCase):
 
     def setUp(self):
         self.writer = writer.Writer('localhost')
+        self.writer.connect()
 
     def test_write_with_auto_increment_id(self):
-        table = 'genetic_algorithms'
-        columns = ['name', 'spec']
+        table = 'test'
+        columns = ['name', 'object', 'time']
+        now = datetime.datetime.now()
+        str_format = '%Y-%m-%d %H:%M:%S'
+        situation_test = situation.Situation()
+        situation_test.set_fitness_function_id(1000)
+        situation_test.set_genome_ranges(
+            short_term=(1, 50),
+            long_term=(50, 100),
+            signal=(1, 50)
+        )
         value = [
-            ['chunk1', 'chunk2'],
-            ['chunk1', 'chunk2'],
+            ['test_1', pickle.dumps(situation_test), now.strftime(str_format)],
+            ['test_2', pickle.dumps(situation_test), now.strftime(str_format)],
         ]
-        self.writer.write_chunk_with_auto_increment_id(table, columns, value)
+        self.writer.write_with_auto_increment_id(table, columns, value)
 
 
 if __name__ == '__main__':
