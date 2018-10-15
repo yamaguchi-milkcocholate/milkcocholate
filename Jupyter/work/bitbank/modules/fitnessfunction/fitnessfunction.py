cf1db5ba309e13f7706eacbba3a2e3257fdbdc07
Yamaguchi Teppei <zuuuubo.tetsu@outlook.jp>
2018-10-16 00:11:54 +0900

    [new] 適応度関数の基底クラスを作成

---
+++ Jupyter/work/bitbank/modules/fitnessfunction/fitnessfunction.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from modules.datamanager import picker
+
+
+class FitnessFunction(ABC):
+    BIT_BANK_CANDLE_STICK = ['5min', '15min', '1hour']
+
+    def __init__(self, candle_type, db_dept):
+        if candle_type in self.BIT_BANK_CANDLE_STICK:
+            self.candlestick = picker.Picker(candle_type).get_candlestick()
+            self.db_dept = db_dept
+        else:
+            raise TypeError('candle type is not found')
+
+    @abstractmethod
+    def calc_fitness(self, geno_type, should_log):
+        pass
