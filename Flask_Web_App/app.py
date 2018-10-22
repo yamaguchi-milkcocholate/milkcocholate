f211937d0bf7fdc855cdc722aaa354fb10d99a01
Yamaguchi Teppei <zuuuubo.tetsu@outlook.jp>
2018-10-22 12:55:39 +0900

    [update] templateを追加

--- Flask_Web_App/app.py
+++ Flask_Web_App/app.py
@@ -1,24 +1,10 @@
-# coding:utf-8
 from flask import Flask, jsonify, request, render_template
 app = Flask(__name__)
 
 
 @app.route("/", methods=['GET'])
 def index():
-    prefectures = list()
-    prefectures.append(('北海度', 1))
-    prefectures.append(('aomori', 2))
-    prefectures.append(('iwate', 3))
-    prefectures.append(('miyagi', 4))
-    prefectures.append(('akita', 5))
-    prefectures.append(('yamagata', 6))
-    prefectures.append(('fukusima', 7))
-    prefectures.append(('ibaraki', 8))
-    prefectures.append(('tochigi', 9))
-    prefectures.append(('gunma', 10))
-    prefectures.append(('chiba', 11))
-    prefectures.append(('kanagawa', 12))
-    return render_template('index.html', prefectures=prefectures)
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
