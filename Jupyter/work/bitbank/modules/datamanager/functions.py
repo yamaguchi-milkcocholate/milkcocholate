c0a55aedfa9646d82ffb1711bfb145fa99dba623
Yamaguchi Teppei <zuuuubo.tetsu@outlook.jp>
2018-10-26 19:28:15 +0900

    [update] functions numpyに書き換え

--- Jupyter/work/bitbank/modules/datamanager/functions.py
+++ Jupyter/work/bitbank/modules/datamanager/functions.py
@@ -17,11 +17,8 @@ def simple_moving_average(candlestick_end, term):
     # ループ回数 = 返すDataFrameの行数
     iteration = len(candlestick_end) - start
     for i in range(iteration):
-        part = candlestick_end.loc[i:start + i]
-        end_sum = 0
-        for index, row in part.iterrows():
-            end_sum += int(row['end'])
-        average_list.append(float(end_sum / term))
+        ends = candlestick_end.loc[i:start + i, 'end'].values
+        average_list.append(float(np.sum(ends) / term))
     return pd.DataFrame(data=average_list, columns=['simple_moving_average'])
 
 
@@ -39,11 +36,8 @@ def exponential_moving_average(candlestick_end, term):
     # 開始位置
     start = term - 1
     # 最初の1回は単純移動平均
-    part = candlestick_end.loc[0:start]
-    end_sum = 0
-    for index, row in part.iterrows():
-        end_sum += int(row['end'])
-    average_list.append(float(end_sum / term))
+    ends = candlestick_end.loc[0:start, 'end'].values
+    average_list.append(float(np.sum(ends) / term))
     # 2回目以降
     for i in range(start + 1, len(candlestick_end)):
         part = candlestick_end.loc[i, 'end']
--- Jupyter/work/bitbank/test/datamanager/test_functions.py
+++ Jupyter/work/bitbank/test/datamanager/test_functions.py
@@ -15,6 +15,7 @@ class TestFunctions(unittest.TestCase):
         list_1 = [1, 1, 1, 4, 4, 10]
         df_1 = pd.DataFrame(data=list_1, columns=['end'])
         result_1 = functions.simple_moving_average(candlestick_end=df_1, term=3)
+        print(result_1)
         # [1, 2, 3, 6]
         self.assertEqual(4, len(result_1))
         self.assertEqual(1, result_1.at[0, 'simple_moving_average'])
@@ -33,6 +34,7 @@ class TestFunctions(unittest.TestCase):
         list_1 = [1, 1, 4, 6, 16, 20]
         df_1 = pd.DataFrame(data=list_1, columns=['end'])
         result_1 = functions.exponential_moving_average(candlestick_end=df_1, term=3)
+        print(result_1)
         # [2, 4, 10, 15]
         self.assertEqual(4, len(result_1))
         self.assertEqual(2, result_1.at[0, 'exponential_moving_average'])
