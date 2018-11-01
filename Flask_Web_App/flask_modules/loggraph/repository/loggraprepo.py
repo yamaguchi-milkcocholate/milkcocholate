de8d4daad83fb1c023da2645c874b513916960eb
Yamaguchi Teppei <zuuuubo.tetsu@outlook.jp>
2018-11-01 23:55:52 +0900

    [update]グラフのデータ構造をjsonそのままに

--- Flask_Web_App/flask_modules/loggraph/repository/loggraprepo.py
+++ Flask_Web_App/flask_modules/loggraph/repository/loggraprepo.py
@@ -12,14 +12,5 @@ class LogGraphRepository:
         result = self._reader(
             table=self.EXPERIMENT_LOGS_TABLE
         ).select().where(['population_id', '=', population_id]).get()
-        plot_data = dict()
-        plot_data['time'] = list()
-        plot_data['position'] = list()
-        plot_data['price'] = list()
-        str_format = '%Y-%m-%d %H:%M:%S'
-        for item in result:
-            plot_data['time'].append(item['logged_at'].strftime(str_format))
-            plot_data['position'].append(item['position'])
-            plot_data['price'].append(item['price'])
-        log_graph = loggraph.LogGraph(population_id=population_id, plot_data=plot_data)
-        return log_graph
+            log_graph = loggraph.LogGraph(population_id=population_id, plot_data=result)
+            return log_graph
