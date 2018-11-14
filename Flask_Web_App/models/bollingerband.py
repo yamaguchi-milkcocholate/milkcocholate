914797d2161e35d0d7775afa3075694f34522eff
Yamaguchi Teppei <zuuuubo.tetsu@outlook.jp>
2018-11-14 10:48:54 +0900

    [update] 複雑なクエリが多くなったのでモデルを設置

---
+++ Flask_Web_App/models/bollingerband.py
@@ -0,0 +1,32 @@
+from flask_modules.loggraph.repository import exptrepo
+from flask_modules.loggraph.repository import poprepo
+from flask_modules.exceptions.dbhost import HostNotFoundException
+
+
+class BollingerBandController:
+
+    def __init__(self, host):
+        self.__host = host
+
+    def __call__(self):
+        """
+        :return:
+        [
+            {'experiment: Experiment, max_fitness: int},
+            {'experiment: Experiment, max_fitness: int},
+        ]
+        """
+        try:
+            pop_repository = poprepo.PopulationRepository(host=self.__host)
+            expt_repository = exptrepo.ExperimentRepository(host=self.__host)
+            experiments = expt_repository.get_bollinger_band()
+            bollingerbands = list()
+            for i in range(len(experiments)):
+                el = dict()
+                el['experiment'] = experiments[i]
+                max_fitness = pop_repository.find_max_fitness(experiment_id=experiments[i].id)
+                el['max_fitness'] = max_fitness
+                bollingerbands.append(el)
+            return bollingerbands
+        except HostNotFoundException:
+            raise
