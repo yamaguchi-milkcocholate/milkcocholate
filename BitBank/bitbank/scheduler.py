823835fbfa5c26f1bed9037b6d31d1add9c7d625
Yamaguchi Teppei <zuuuubo.tetsu@outlook.jp>
2018-12-04 09:26:21 +0900

    [update] Scheduler関係の整備とテスト

--- BitBank/bitbank/scheduler.py
+++ BitBank/bitbank/scheduler.py
@@ -2,6 +2,7 @@ import sched
 import datetime
 import time
 import sys
+from pytz import timezone
 from bitbank.exceptions.schedcancel import SchedulerCancelException
 
 
@@ -15,14 +16,14 @@ class Scheduler:
         self.__schedule()
 
     def __schedule(self):
-        now = datetime.datetime.now()
+        now = datetime.datetime.now(timezone('UTC')).astimezone(timezone('Asia/Tokyo'))
         now_5min = now + datetime.timedelta(minutes=5)
         now_a_week = now + datetime.timedelta(days=7)
-        print('Start at' + now.strftime('%Y-%m-%d %H:%M:%S'))
+        print('Start at ' + now.strftime('%Y-%m-%d %H:%M:%S'))
 
         time_i = int(time.mktime(now.timetuple()))
         span = int(time.mktime(now_5min.timetuple()) - time_i)
-        a_week = int(time.mktime(now_a_week.timetuple()) - time_i)
+        a_week = int(time.mktime(now_a_week.timetuple()))
         while time_i <= a_week:
             self.__scheduler.enterabs(
                 time_i,
@@ -32,11 +33,12 @@ class Scheduler:
             )
             time_i += span
         self.__scheduler.run()
+        print('Finish at ' + datetime.datetime.fromtimestamp(time_i - span).strftime('%Y-%m-%d %H:%M:%S'))
 
     def __processing(self, *args):
         try:
             print('Execute at ' + args[0])
-            self.__runner.processing()
+            self.__runner()
         except SchedulerCancelException as e:
             # スケジューラをキャンセル
             print(e.get_message())
