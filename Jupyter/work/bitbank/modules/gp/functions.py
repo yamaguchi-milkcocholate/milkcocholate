4536095f1e38165ab7d4360a4da66ffa7d6ea740
Yamaguchi Teppei <zuuuubo.tetsu@outlook.jp>
2018-12-19 22:10:48 +0900

    [update] 波形テンプレート実行

--- Jupyter/work/bitbank/modules/gp/functions.py
+++ Jupyter/work/bitbank/modules/gp/functions.py
@@ -1,5 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+
+PATTERN = 9
+INC = 1
 
 
 def show_wave(**kwargs):
@@ -37,68 +41,149 @@ def template(size, number):
     if size % 2 == 0:
         size += 1
 
-    inc = 1
-
     if number is 0:
         data = np.zeros(size)
         return data
     elif number is 1:
-        data = np.arange(-1 * (size / 2), size / 2)
-        data = data * inc
+        data = np.arange(-1 * size / 2, size / 2)
+        data = data * INC
         return data
     elif number is 2:
+        data = np.arange(-1 * size / 2, size / 2)
+        data = -1 * data * INC
+        return data
+    elif number is 3:
         # 前半の右肩上がり
         data = np.arange(-1 * size / 2, 0)
         data = data - np.average(data)
-        data = data * inc
+        data = data * INC
         tmp = np.zeros_like(data)[:-1]
         tmp = tmp + data[-1]
         data = np.append(data, tmp)
         return data
-    elif number is 3:
+    elif number is 4:
         # 後半の右肩下がり
         data = np.arange(0, size / 2)
         data = data - np.average(data)
-        data = -1 * data * inc
+        data = -1 * data * INC
         tmp = np.zeros_like(data)[:-1]
         tmp = tmp + data[0]
         data = np.append(tmp, data)
         return data
-    elif number is 4:
+    elif number is 5:
         # 前半右肩下がり
         data = np.arange(0, size / 2)
         data = data - np.average(data)
-        data = -1 * data * inc
+        data = -1 * data * INC
         tmp = np.zeros_like(data)[:-1]
         tmp = tmp + data[-1]
         data = np.append(data, tmp)
         return data
-    elif number is 5:
+    elif number is 6:
         # 前半の右肩上がり
         data = np.arange(-1 * size / 2, 0)
         data = data - np.average(data)
-        data = data * inc
+        data = data * INC
         tmp = np.zeros_like(data)[:-1]
         tmp = tmp + data[0]
         data = np.append(tmp, data)
-        return data
-    elif number is 6:
-        # 上に凸
-        data = np.arange(-1 * size / 2, 0)
-        data = data - np.average(data)
-        data = data * inc
-        tmp = np.arange(0, size / 2)
-        tmp = tmp - np.average(tmp)
-        tmp = -1 * tmp * inc
-        data = np.append(data, tmp)
         return data
     elif number is 7:
         # 上に凸
         data = np.arange(-1 * size / 2, 0)
         data = data - np.average(data)
-        data = data * inc
+        data = data * INC
         tmp = np.arange(0, size / 2)
         tmp = tmp - np.average(tmp)
-        tmp = -1 * tmp * inc
-        data = np.append(tmp, data)
+        tmp = -1 * tmp * INC
+        data = np.append(data, tmp[1:])
         return data
+    elif number is 8:
+        # 下に凸
+        data = np.arange(-1 * size / 2, 0)
+        data = data - np.average(data)
+        data = data * INC
+        tmp = np.arange(0, size / 2)
+        tmp = tmp - np.average(tmp)
+        tmp = -1 * tmp * INC
+        data = np.append(tmp, data[:-1])
+        return data
+
+
+def normalize(data):
+    tmp = data * data
+    tmp = np.sum(tmp)
+    # 原点ベクトルはそのまま返す
+    if tmp == 0:
+        return data
+    tmp = math.sqrt(tmp)
+    data = data / tmp
+    return data
+
+
+def cosine_similarity(x, y):
+    """
+    コサイン類似度を返す関数
+    :param x: numpy
+    :param y: numpy
+    :return: numpy | float
+    """
+    if len(x) != len(y):
+        raise Exception()
+
+    x_norm = np.linalg.norm(x)
+    y_norm = np.linalg.norm(y)
+    sim = np.sum(x * y)
+    sim = sim / (x_norm * y_norm + 0.00000000001)
+    return sim
+
+
+def correlation_coefficient(x, y):
+    """
+    ピアソン相関係数
+    :param x: numpy
+    :param y: numpy
+    :return: numpy
+    """
+    if len(x) != len(y):
+        print(x)
+        print(y)
+        raise Exception()
+
+    x = x - np.average(x)
+    y = y - np.average(y)
+    return cosine_similarity(x, y)
+
+
+def deviation_pattern_similarity(x, y):
+    """
+    偏差パターン類似度
+    :param x: numpy
+    :param y: numpy
+    :return: numpy
+    """
+    if len(x) != len(y):
+        raise Exception()
+
+    m = x + y / 2
+    x = x - m
+    y = y - m
+    return cosine_similarity(x, y)
+
+
+def pattern_analysis(x):
+    """
+    パターン分けする関数
+    :param x: numpy
+    :return: integer
+    """
+    size = len(x)
+    sim = list()
+    if size % 2 == 0:
+        raise Exception()
+
+    for i in range(PATTERN):
+        tpl = template(size=size, number=i)
+        sim.append(correlation_coefficient(tpl, x))
+        print('pattern', i, ':', sim[-1])
+    return sim.index(max(sim))
--- Jupyter/work/bitbank/test/gp/test_functions.py
+++ Jupyter/work/bitbank/test/gp/test_functions.py
@@ -1,15 +1,22 @@
 import sys
 import os
 import unittest
+import random
 import numpy as np
 sys.path.append(os.pardir + '/../')
 from modules.gp.functions import *
+from modules.datamanager.picker import Picker
 
 
 class TestFunctions(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.picker = Picker(
+            '5min',
+            use_of_data='validation',
+            coin='xrp',
+            is_inclination=True
+        )
 
     def tearDown(self):
         pass
@@ -17,14 +24,14 @@ class TestFunctions(unittest.TestCase):
     def test_show_wave(self):
         data = np.random.rand(20)
         data = 37.0 + data * 2
-        #show_wave(data=data)
+        # show_wave(data=data)
 
     def test_diff(self):
         data = np.random.rand(20)
         data = 37.0 + data * 2
         f = data
         data = diff(data=data)
-        #show_wave(f=f, df=data)
+        # show_wave(f=f, df=data)
 
     def test_template(self):
         data_0 = template(9, 0)
@@ -35,6 +42,7 @@ class TestFunctions(unittest.TestCase):
         data_5 = template(9, 5)
         data_6 = template(9, 6)
         data_7 = template(9, 7)
+        """
         show_wave(
             template_0=data_0,
             template_1=data_1,
@@ -45,6 +53,105 @@ class TestFunctions(unittest.TestCase):
             template_6=data_6,
             template_7=data_7
         )
+        """
+        data_0 = diff(data=data_0)
+        data_1 = diff(data=data_1)
+        data_2 = diff(data=data_2)
+        data_3 = diff(data=data_3)
+        data_4 = diff(data=data_4)
+        data_5 = diff(data=data_5)
+        data_6 = diff(data=data_6)
+        data_7 = diff(data=data_7)
+        """
+        show_wave(
+            template_0=data_0,
+            template_1=data_1,
+            template_2=data_2,
+            template_3=data_3,
+            template_4=data_4,
+            template_5=data_5,
+            template_6=data_6,
+            template_7=data_7
+        )
+        """
+
+    def test_candlestick(self):
+        candlestick = self.picker.get_candlestick()
+        candlestick = candlestick.loc[:10].end.values
+        data = diff(data=candlestick)
+        data_7 = template(9, 7)
+        diff_7 = diff(data=data_7)
+        """
+        show_wave(
+            candlestick=candlestick,
+            data=data,
+            template=data_7,
+            template_diff=diff_7
+        )
+        """
+
+    def test_normalize(self):
+        candlestick = self.picker.get_candlestick()
+        candlestick = candlestick.loc[:10].end.values
+        data = normalize(data=candlestick)
+        data_7 = template(9, 2)
+        diff_7 = normalize(data=data_7)
+        """
+        show_wave(
+            candlestick=candlestick,
+            data=data,
+            template=data_7,
+            template_diff=diff_7
+        )
+        """
+
+    def test_cosine_similarity(self):
+        candlestick = self.picker.get_candlestick()
+        candlestick = candlestick.loc[:10].end.values
+        data = template(10, 2)
+        sim = cosine_similarity(data, candlestick)
+        print(sim)
+        x = np.asarray([1, 0, 0])
+        y = np.asarray([0.5, 0, 0])
+        sim = cosine_similarity(x, y)
+        self.assertAlmostEqual(1, sim)
+
+    def test_correlation_coefficient(self):
+        candlestick = self.picker.get_candlestick()
+        candlestick = candlestick.loc[:10].end.values
+        data = template(10, 2)
+        sim = correlation_coefficient(data, candlestick)
+        print(sim)
+        x = np.asarray([1, 0, 0])
+        y = np.asarray([0.5, 0, 0])
+        sim = correlation_coefficient(x, y)
+        self.assertAlmostEqual(1, sim)
+
+    def test_deviation_pattern_similarity(self):
+        candlestick = self.picker.get_candlestick()
+        candlestick = candlestick.loc[:10].end.values
+        data = template(10, 2)
+        sim = deviation_pattern_similarity(data, candlestick)
+        print(sim)
+        x = np.asarray([1, 0, 0])
+        y = np.asarray([0.5, 0, 0])
+        sim = deviation_pattern_similarity(x, y)
+        self.assertAlmostEqual(1, sim)
+
+    def test_pattern_analysis(self):
+        candlestick = self.picker.get_candlestick()
+        term = 10
+        for i in range(0):
+            start = random.randint(0, len(candlestick) - term)
+            data = candlestick.loc[start:start + term].end.values
+            print()
+            sim = pattern_analysis(data)
+            print(sim)
+            tpl = template(size=len(data), number=sim)
+            show_wave(
+                data=tpl,
+                candlestick=data
+            )
 
 
 if __name__ == '__main__':
