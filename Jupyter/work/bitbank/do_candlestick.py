d3f5574af7fb011665e5dd754fc6ddced09e99e7
Yamaguchi Teppei <zuuuubo.tetsu@outlook.jp>
2019-01-06 15:07:32 +0900

    [update]

--- Jupyter/work/bitbank/do_candlestick.py
+++ Jupyter/work/bitbank/do_candlestick.py
@@ -2,11 +2,11 @@ from modules.datamanager import savecandlestick
 
 
 save_candle_data = savecandlestick.SaveCandlestick()
-start_day = '20180601'
-finish_day = '20181231'
+start_day = '20190101'
+finish_day = '20190105'
 pair = 'xrp_jpy'
 timespace = '15min'
-folder = 'validation_xrp'
+folder = 'data_xrp'
 save_candle_data.save_candlestick(
     timespace=timespace,
     start_day=start_day,
