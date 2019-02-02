992783125b4d22bfb7e4ba65171791f78501ab94
Yamaguchi Teppei <zuuuubo.tetsu@outlook.jp>
2019-02-02 17:04:47 +0900

    [update]

--- BitBank/do.py
+++ BitBank/do.py
@@ -9,23 +9,9 @@ api_key = input()
 print('api_secret: ', end='')
 api_secret = input()
 
-print('host: ', end='')
-host = input()
-
-print('population id: ', end='')
-population_id = input()
-population_id = int(population_id)
-
-print('genome id: ', end='')
-genome_id = input()
-genome_id = int(genome_id)
-
 adviser = ZigZagAdviser(init_max_high=33.5, init_min_low=33.0)
 
 bot = Bot(
-    host=host,
-    population_id=population_id,
-    genome_id=genome_id,
     adviser=adviser,
     pair='xrp_jpy',
     api_key=api_key,
