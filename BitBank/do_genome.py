e1ac5dcab5b9b94c8ec4a59ae1c1066e342f40da
Yamaguchi Teppei <zuuuubo.tetsu@outlook.jp>
2019-03-02 14:03:18 +0900

    [update]

--- BitBank/do_genome.py
+++ BitBank/do_genome.py
@@ -4,8 +4,6 @@ import sys
 from pprint import pprint
 sys.setrecursionlimit(100000)
 
-gp = read_file(directory='15min/training/gp_02.pkl')
+gp = read_file(directory='15min/training/gp_next_03.pkl')
 genome = gp.get_elite_genome()
-r, rr, sr, er = genome.true_route()
-genome.pruning_true_route(success_route=sr, keep=3, depth=10)
-genome.show_tree_map(name='genome_tree_2')
+genome.show_tree_map(name='gp_next_03')
